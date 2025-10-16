ca201ea740e67b8f19c2e829eb75c666f1ac8a40
Othmane El Ouajdi <oelouajdi@hotmail.com>
2025-10-16 21:29:59 +0200

    Initial commit

---
+++ src/main/java/com/salon/coiffure/CoiffureApplication.java
@@ -0,0 +1,13 @@
+package com.salon.coiffure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoiffureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoiffureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salon/coiffure/CoiffureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.coiffure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoiffureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
